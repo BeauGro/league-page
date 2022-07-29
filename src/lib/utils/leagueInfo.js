fdab5733b19be076f9df52cf89dcaf9bdfbc7afb
BeauGro <110256775+BeauGro@users.noreply.github.com>
2022-07-29 16:17:02 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,7 +32,7 @@ export const managers = [
       "bio": "They call me Back 2 Back Beau. Word on the street is a 3-Peat is impossible, but the street is wrong on this one. 2020 trophy in my left hand, 2021 trophy in my right hand, I might have a hard time picking up my phone to set my lineups this year.",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "GB", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "nhkp9874", // Can be anything (usually your rival's name)
